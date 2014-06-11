411be14015c82b58c7d563fa40d38959d85bb11e
Tobias Blankenhagen <tobiasblankenhagen@gmail.com>
2014-06-11 19:04:53 +0200

    Finished

--- MainGui/Form1.Designer.cs
+++ MainGui/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MainGui
 {
-    partial class Form1
+    partial class MainGui
     {
         /// <summary>
         /// Erforderliche Designervariable.
@@ -28,12 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnGenerateKey = new System.Windows.Forms.Button();
+            this.chkLittleLetters = new System.Windows.Forms.CheckBox();
+            this.chkBigLetters = new System.Windows.Forms.CheckBox();
+            this.chkNumbers = new System.Windows.Forms.CheckBox();
+            this.chkSpecialcharacters = new System.Windows.Forms.CheckBox();
+            this.txtGeneratedKey = new System.Windows.Forms.TextBox();
+            this.txtCountCharacters = new System.Windows.Forms.TextBox();
+            this.lblCountCharacters = new System.Windows.Forms.Label();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGenerateKey
+            // 
+            this.btnGenerateKey.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGenerateKey.Location = new System.Drawing.Point(317, 55);
+            this.btnGenerateKey.Name = "btnGenerateKey";
+            this.btnGenerateKey.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerateKey.TabIndex = 0;
+            this.btnGenerateKey.Text = "Generieren";
+            this.btnGenerateKey.UseVisualStyleBackColor = true;
+            this.btnGenerateKey.Click += new System.EventHandler(this.btnGenerateKey_Click);
+            // 
+            // chkLittleLetters
+            // 
+            this.chkLittleLetters.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.chkLittleLetters.AutoSize = true;
+            this.chkLittleLetters.Location = new System.Drawing.Point(12, 61);
+            this.chkLittleLetters.Name = "chkLittleLetters";
+            this.chkLittleLetters.Size = new System.Drawing.Size(97, 17);
+            this.chkLittleLetters.TabIndex = 1;
+            this.chkLittleLetters.Text = "kl. Buchstaben";
+            this.chkLittleLetters.UseVisualStyleBackColor = true;
+            // 
+            // chkBigLetters
+            // 
+            this.chkBigLetters.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.chkBigLetters.AutoSize = true;
+            this.chkBigLetters.Location = new System.Drawing.Point(12, 84);
+            this.chkBigLetters.Name = "chkBigLetters";
+            this.chkBigLetters.Size = new System.Drawing.Size(100, 17);
+            this.chkBigLetters.TabIndex = 2;
+            this.chkBigLetters.Text = "Gr. Buchstaben";
+            this.chkBigLetters.UseVisualStyleBackColor = true;
+            // 
+            // chkNumbers
+            // 
+            this.chkNumbers.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.chkNumbers.AutoSize = true;
+            this.chkNumbers.Location = new System.Drawing.Point(118, 61);
+            this.chkNumbers.Name = "chkNumbers";
+            this.chkNumbers.Size = new System.Drawing.Size(53, 17);
+            this.chkNumbers.TabIndex = 3;
+            this.chkNumbers.Text = "Ziffen";
+            this.chkNumbers.UseVisualStyleBackColor = true;
+            // 
+            // chkSpecialcharacters
+            // 
+            this.chkSpecialcharacters.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.chkSpecialcharacters.AutoSize = true;
+            this.chkSpecialcharacters.Location = new System.Drawing.Point(118, 84);
+            this.chkSpecialcharacters.Name = "chkSpecialcharacters";
+            this.chkSpecialcharacters.Size = new System.Drawing.Size(97, 17);
+            this.chkSpecialcharacters.TabIndex = 4;
+            this.chkSpecialcharacters.Text = "Sonderzeichen";
+            this.chkSpecialcharacters.UseVisualStyleBackColor = true;
+            // 
+            // txtGeneratedKey
+            // 
+            this.txtGeneratedKey.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtGeneratedKey.Enabled = false;
+            this.txtGeneratedKey.Location = new System.Drawing.Point(12, 12);
+            this.txtGeneratedKey.MaxLength = 64;
+            this.txtGeneratedKey.Name = "txtGeneratedKey";
+            this.txtGeneratedKey.Size = new System.Drawing.Size(380, 20);
+            this.txtGeneratedKey.TabIndex = 5;
+            this.txtGeneratedKey.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtCountCharacters
+            // 
+            this.txtCountCharacters.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtCountCharacters.Location = new System.Drawing.Point(221, 81);
+            this.txtCountCharacters.MaxLength = 2;
+            this.txtCountCharacters.Name = "txtCountCharacters";
+            this.txtCountCharacters.Size = new System.Drawing.Size(66, 20);
+            this.txtCountCharacters.TabIndex = 6;
+            // 
+            // lblCountCharacters
+            // 
+            this.lblCountCharacters.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblCountCharacters.AutoSize = true;
+            this.lblCountCharacters.Location = new System.Drawing.Point(218, 61);
+            this.lblCountCharacters.Name = "lblCountCharacters";
+            this.lblCountCharacters.Size = new System.Drawing.Size(75, 13);
+            this.lblCountCharacters.TabIndex = 7;
+            this.lblCountCharacters.Text = "Anzal Zeichen";
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCopy.Location = new System.Drawing.Point(317, 84);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 8;
+            this.btnCopy.Text = "Kopieren";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // MainGui
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(404, 113);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.lblCountCharacters);
+            this.Controls.Add(this.txtCountCharacters);
+            this.Controls.Add(this.txtGeneratedKey);
+            this.Controls.Add(this.chkSpecialcharacters);
+            this.Controls.Add(this.chkNumbers);
+            this.Controls.Add(this.chkBigLetters);
+            this.Controls.Add(this.chkLittleLetters);
+            this.Controls.Add(this.btnGenerateKey);
+            this.Name = "MainGui";
+            this.Text = "WlanKey Generator";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainGui_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGenerateKey;
+        private System.Windows.Forms.CheckBox chkLittleLetters;
+        private System.Windows.Forms.CheckBox chkBigLetters;
+        private System.Windows.Forms.CheckBox chkNumbers;
+        private System.Windows.Forms.CheckBox chkSpecialcharacters;
+        private System.Windows.Forms.TextBox txtGeneratedKey;
+        private System.Windows.Forms.TextBox txtCountCharacters;
+        private System.Windows.Forms.Label lblCountCharacters;
+        private System.Windows.Forms.Button btnCopy;
     }
 }
 
--- MainGui/Form1.cs
+++ MainGui/Form1.cs
@@ -10,11 +10,142 @@ using System.Windows.Forms;
 
 namespace MainGui
 {
-    public partial class Form1 : Form
+    /// ============================================================================================================================
+    public partial class MainGui : Form
     {
-        public Form1()
+        #region Konstanten
+
+        /// ------------------------------------------------------------------------------------------------------------------------
+        /// <summary>Konstante für "Anzahl Zeichen" in den Settings</summary>
+        private const string CharacterCountSection = "DefaultCharacterCount";
+
+        #endregion Konstanten
+
+        #region Konstruktoren
+
+        /// ------------------------------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        public MainGui()
         {
             InitializeComponent();
+            SetDefaults();
         }
+
+        #endregion Konstruktoren
+
+        #region Methoden
+
+        /// ------------------------------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Setzt die Anfangswerte für die Checkboxen und die Zeichenanzahl
+        /// </summary>
+        private void SetDefaults()
+        {
+            chkBigLetters.Checked = true;
+            chkLittleLetters.Checked = true;
+            chkNumbers.Checked = true;
+            chkSpecialcharacters.Checked = false;
+
+            txtCountCharacters.Text = ReadSettings(CharacterCountSection);
+        }
+
+        /// ------------------------------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Erzeugt den Key anhand der Einstellungen
+        /// </summary>
+        /// <returns>Der generierte Key</returns>
+        private string GenerateKey()
+        {
+            Random random = new Random();
+            string Key = "";
+            string characters = "";
+
+            if (chkLittleLetters.Checked)
+                characters += "abcdefghijklmnopqrstuvwxyz";
+            if (chkBigLetters.Checked)
+                characters += "ABCDEFGHIJKLMOPQRSTUVWXYZ";
+            if (chkNumbers.Checked)
+                characters += "0123456789";
+            if (chkSpecialcharacters.Checked)
+                characters += "!§$%&/()=?";
+            List<char> charList = characters.ToList();
+            int characterCount = int.Parse(txtCountCharacters.Text);
+            for (int count = 0; count < characterCount; count++)
+            {
+                Key += charList[random.Next(0, characters.Length)];
+            }
+            return Key;
+        }
+
+        /// ------------------------------------------------------------------------------------------------------------------------
+        /// summary>
+        /// Schreibt die übergebenen Werte in die Settings
+        /// </summary>
+        /// <param name="section">Section unter der der Wert gespeichert werden soll, Einzigartig</param>
+        /// <param name="value">Wert der gespeichert werden soll</param>
+        private static void WriteSettings(String section, String value)
+        {
+            if (section.Equals(CharacterCountSection))
+            {
+                Properties.Settings.Default.DefaultCharacterCount = value;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        /// ------------------------------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Liest den Wert aus der übergebenen Section
+        /// </summary>
+        /// <param name="section">Section die gelesen werden soll</param>
+        /// <returns>Den der Wert der Section oder String.Empty</returns>
+        private static String ReadSettings(String section)
+        {
+            if (section.Equals(CharacterCountSection))
+                return Properties.Settings.Default.DefaultCharacterCount;
+            return string.Empty;
+        }
+
+        #endregion Methoden
+
+        #region Eventhandler
+
+        /// ------------------------------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Eventhandler: Erzeugt den Key
+        /// </summary>
+        /// <param name="sender">object</param>
+        /// <param name="e">EventArgs</param>
+        private void btnGenerateKey_Click(object sender, EventArgs e)
+        {
+            txtGeneratedKey.Text = GenerateKey();
+            txtGeneratedKey.Enabled = true;
+        }
+
+        /// ------------------------------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Eventhandler: Kopiert den Inhalt des Keyfensters in die Zwischenablage
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Clipboard.SetDataObject(txtGeneratedKey.Text);
+        }
+
+        /// ------------------------------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Speichert beim Schließen des Programms die zuletzt eingegebene Zeichenanzahl in die Settings
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainGui_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            WriteSettings(CharacterCountSection, txtCountCharacters.Text);
+        }
+
+        #endregion Eventhandler
+
     }
 }
